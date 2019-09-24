2b043ba58759d630f870ce163917b2454c7f97c5
Peizhao Hu <ph@cs.rit.edu>
2019-09-23 23:06:50 -0400

    Add scripts for initializing container.

--- docker/initContainer.bash
+++ docker/initContainer.bash
@@ -2,3 +2,8 @@
 
 source /etc/profile
 /etc/init.d/ssh start
+
+CONTAINERS_IP=$(cat myDockerHostFile | cut -f1 -d' ')
+for ip in ${CONTAINERS_IP[@]}; do
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $ip "uptime"
+done
