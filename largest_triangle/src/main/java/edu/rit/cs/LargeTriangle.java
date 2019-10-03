07c4d8d39dd8df04a1156adea8000baf13b428f1
Peizhao Hu <ph@cs.rit.edu>
2019-10-03 17:40:43 -0400

    update the example code for testing the random number generator

--- largest_triangle/src/main/java/edu/rit/cs/LargeTriangle.java
+++ largest_triangle/src/main/java/edu/rit/cs/LargeTriangle.java
@@ -6,11 +6,13 @@ public class LargeTriangle
 
     public static void main( String[] args ) {
         int numPoints = 100;
-        RandomPoints rndPoints = new RandomPoints(numPoints, 5, 123);
+        RandomPoints rndPoints = new RandomPoints(numPoints, 100, 142857);
+        int idx=1;
         Point p;
         while(rndPoints.hasNext()) {
             p = rndPoints.next();
-            System.out.println("x=" + p.getX() + " y=" + p.getY());
+            System.out.println(idx+": x=" + p.getX() + " y=" + p.getY());
+            idx++;
         }
     }
 }
